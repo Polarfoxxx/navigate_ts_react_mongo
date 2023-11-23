f1c896541c1cb20c415438610fa686afca7f912f
suchovsky.michal@gmail.com <suchovsky.michal@gmail.com>
2023-11-23 11:43:01 +0100

    update OnClickBussinesSearcheContent.tsx

--- client/src/module/Control/Components/PoputsContentControl/OnClickBussinesSearcheContent/OnClickBussinesSearcheContent.tsx
+++ client/src/module/Control/Components/PoputsContentControl/OnClickBussinesSearcheContent/OnClickBussinesSearcheContent.tsx
@@ -4,7 +4,23 @@ function OnClickBussinesSearcheContent(): JSX.Element {
     return(
         <div className="bussinecContent">
             <div className="bussinecContent_header">
-                    hi
+                  <div className='bussinesHeaderLogo'>
+                  
+                  </div>
+                 <div className='bussinesHeaderName'>
+                     <h2>nzovs</h2>
+                  </div>
+            </div>
+            <div className='bussinesContentBody'>
+               <div className='bussinesContentBodyDistance'>
+                 <p>45</p><span>k</span>
+               </div>
+               <div className>
+               
+               </div>
+            </div>
+            <div className='bussinesContentFooter'>
+            
             </div>
         </div>
     );
