a63fd0f527b4f99322bccf638d285a898402a610
suchovskyMichal <suchovsky.michal@gmail.com>
2024-01-26 20:00:56 +0100

    the mapTraffic fixxing issies

--- client/src/module/IncidentsToMaps/Components/MapTraffic/MapTraffic.tsx
+++ client/src/module/IncidentsToMaps/Components/MapTraffic/MapTraffic.tsx
@@ -36,21 +36,47 @@ function MapTraffic(): null {
     /* funkcia pre stahovanie dat */
     async function fetchImageLayer(DATA_FOR_MAPTRAFF: any) {
         try {
-            const FETCH_DATA = await mapTaffic_API(DATA_FOR_MAPTRAFF);
-            const IMAGE_LAYER = FETCH_DATA;
+          const FETCH_DATA = await mapTaffic_API(DATA_FOR_MAPTRAFF);
+      
+          // Přidána kontrola pro undefined
+          if (FETCH_DATA !== undefined) {
+            // Převod ArrayBuffer na base64 řetězec
+            const base64String = arrayBufferToBase64(FETCH_DATA);
+      
+            // Vytvoření URL pro obrázek
+            const IMAGE_LAYER = `data:image/png;base64,${base64String}`;
+      
+            // Ověřte, že `imageLayer`, `MAP`, a `setImageLayer` jsou definovány
+            if (imageLayer && MAP) {
+              MAP.removeLayer(imageLayer);
+            }
+      
             const NEW_IMAGE_LAYER = L.imageOverlay(IMAGE_LAYER, MAP.getBounds());
-
-            if (imageLayer) {
-                MAP.removeLayer(imageLayer);
-            };
-
-            NEW_IMAGE_LAYER.addTo(MAP);
-            setImageLayer(NEW_IMAGE_LAYER);
-
+      
+            // Ověřte, že `MAP` a `setImageLayer` jsou definovány
+            if (MAP) {
+              NEW_IMAGE_LAYER.addTo(MAP);
+              setImageLayer(NEW_IMAGE_LAYER);
+            } else {
+              console.error("MAP not available");
+            }
+          } else {
+            console.error("Data not available");
+          }
+      
         } catch (error) {
-            console.error(error);
-        };
-    };
+          console.error(error);
+        }
+      }
+      
+      // Funkce pro převod ArrayBuffer na base64 řetězec
+      function arrayBufferToBase64(buffer: ArrayBuffer): string {
+        const binaryArray = new Uint8Array(buffer);
+        const binary = Array.from(binaryArray); // nebo: const binary = Array.prototype.slice.call(binaryArray);
+      
+        return btoa(binary.map((num) => String.fromCharCode(num)).join(''));
+      }
+      
 
     return null;
 };
